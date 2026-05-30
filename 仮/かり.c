b317083ba8f0e5ca6e5a8023485bcc69a37812b8
kanaha0610 <kanahakikannriko@fuji.waseda.jp>
2026-05-30 14:06:59 +0900

    first commit

---
+++ 仮/かり.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void){
+    int k, i, r;
+    printf("row:");
+    scanf("%d",&r);
+
+i = 1;
+k = 0;
+    while(i<=r){
+        while(k<i){
+
+        printf("x");
+        k=k+1;
+    }
+    k=0;
+        printf("\n");
+        i=i+1;
+     }
+    }
